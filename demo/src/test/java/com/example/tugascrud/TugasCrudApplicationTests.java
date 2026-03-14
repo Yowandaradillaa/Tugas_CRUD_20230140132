4b76cfe408ce11325c9046316f628d6c54c05535
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-03-14 23:01:22 +0700

    Initial commit

---
+++ demo/src/test/java/com/example/tugascrud/TugasCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tugascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
