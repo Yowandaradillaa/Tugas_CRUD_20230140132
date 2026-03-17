4f7d4461b8d25db1320305c30ee6469d6dfe07ac
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-03-17 19:53:14 +0700

    menambahkan validasi

---
+++ demo/src/main/java/com/example/tugascrud/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.tugascrud.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
---
+++ demo/src/main/java/com/example/tugascrud/exception/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.tugascrud.exception;
+
+public class ResourceNotFoundException {
+}
