6b2ffeddbb701b355a7d038918dc389f23f70171
Yowandaradillaa <yowandaradilla13@gmail.com>
2026-03-17 19:55:11 +0700

    memindahkan file

--- demo/src/main/java/com/example/tugascrud/TugasCrudApplication.java
+++ demo/src/main/java/com/example/tugascrud/TugasCrudApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TugasCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TugasCrudApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TugasCrudApplication.class, args);
+    }
 }
